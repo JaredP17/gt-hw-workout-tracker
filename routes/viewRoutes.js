a5d565799b4fa631d23b57d410a882de7c82ec88
Jared Porter <jaredp17@me.com>
2020-07-28 23:03:56 -0400

    Refactor router in view routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,5 @@
 const path = require("path");
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));

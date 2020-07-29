58e505c9eae3a624ae5578622c295229cbbf7387
Jared Porter <jaredp17@me.com>
2020-07-28 22:10:30 -0400

    Added workout model from 1st attempt at homework.

---
+++ models/Workout.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter the type of workout.",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter the name of workout.",
+        },
+        duration: {
+          type: Number,
+          required: "Enter the duration of workout.",
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce(
+    (totalDuration, exercise) => totalDuration + exercise.duration,
+    0
+  );
+});
+
+module.exports = {
+  Workout: mongoose.model("Workout", WorkoutSchema),
+};

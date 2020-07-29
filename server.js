13c89ac4ab760831030ff8b0cb13325c5b3ff67f
Jared Porter <jaredp17@me.com>
2020-07-28 21:59:54 -0400

    Included view routes in server.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // // routes
 // app.use(require("./routes/apiRoutes.js"));
-// app.use(require("./routes/viewRoutes.js"));
+app.use(require("./routes/viewRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
